d4b389880f603c0add0f127849b24ec4cbe15592
RasssJ <rasmus.jalakas11@gmail.com>
2023-05-29 22:50:17 +0300

    Create GlobalUsings.cs

---
+++ MonkeyFinder/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using MonkeyFinder.Model;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
