ed4dbb46e4350176b9978b87645197e171520e53
RasssJ <rasmus.jalakas11@gmail.com>
2023-05-31 00:05:05 +0300

    Open Map Integration

--- MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,14 +1,40 @@
-﻿using ViewModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MonkeyFinder.Model;
+using MonkeyFinder.ViewModel;
+using ViewModel;
 
 namespace MonkeyFinder.ViewModel;
 
 [QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    public MonkeyDetailsViewModel()
+    IMap map;
+    public MonkeyDetailsViewModel(IMap map)
     {
+        this.map = map;
     }
 
-   // [ObservableProperty]
+   [ObservableProperty]
     Monkey monkey;
+
+    [RelayCommand]
+    async Task OpenMapAsync()
+    {
+        try
+        {
+            await map.OpenAsync(Monkey.Latitude, Monkey.Longitude,
+                new MapLaunchOptions
+                {
+                    Name = Monkey.Name,
+                    NavigationMode = NavigationMode.None,
+                });
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Unable to open map: {ex.Message}", "OK");
+        }
+    }
 }
