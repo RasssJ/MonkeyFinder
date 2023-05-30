6a784a2eed13319f8cf69a53ea948ba722e4b7eb
RasssJ <rasmus.jalakas11@gmail.com>
2023-05-30 23:47:59 +0300

    Geolocation & Permissions Implementation

--- MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -8,13 +8,58 @@ public partial class MonkeysViewModel : BaseViewModel
 {
         MonkeyService monkeyService;
         public ObservableCollection<Monkey> Monkeys { get; set; } = new();
+
     IConnectivity connectivity;
+    IGeolocation geolocation;
 
     public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
             Title = "Monkey Finder";
             this.monkeyService = monkeyService;
             this.connectivity = connectivity;
+            this.geolocation = geolocation;
+    }
+
+    [RelayCommand]
+    async Task GetClosestMonkeyAsync()
+    {
+        if (IsBusy || Monkeys.Count == 0)
+            return;
+
+        try
+        {
+            var location = await geolocation.GetLastKnownLocationAsync();
+            if (location is null)
+            {
+                location = await geolocation.GetLocationAsync(
+                                   new GeolocationRequest
+                                   {
+                                       DesiredAccuracy = GeolocationAccuracy.Medium,
+                                       Timeout = TimeSpan.FromSeconds(30),
+                                   });
+
+            }
+
+            if (location is null)
+                return;
+
+            var first = Monkeys.OrderBy(m =>
+                location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles)
+                ).FirstOrDefault();
+
+            if (first is null)
+                return;
+
+            await Shell.Current.DisplayAlert("Closest Monkey",
+                $"{first.Name} in {first.Location}", "OK");
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Unable to get closest monkeys: {ex.Message}", "OK");
+        }
     }
 
     [RelayCommand]
