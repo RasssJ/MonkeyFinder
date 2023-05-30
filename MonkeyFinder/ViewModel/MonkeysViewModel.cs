71e938b82dad1bd611d48c56dd650f47055b95a7
RasssJ <rasmus.jalakas11@gmail.com>
2023-05-30 19:04:33 +0300

    Add code to MonkeysViewModel.cs

--- MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Services;
+using System.Linq.Expressions;
 
-namespace MonkeyFinder.ViewModel
+namespace ViewModel;
+public partial class MonkeysViewModel : BaseViewModel
 {
-    class MonkeysViewModel
+        MonkeyService monkeyService;
+        public ObservableCollection<Monkey> Monkeys { get; set; } = new();
+        public MonkeysViewModel(MonkeyService monkeyService)
+        {
+            Title = "Monkey Finder";
+            this.monkeyService = monkeyService;
+        }
+    async Task GetMonkeyAsync()
     {
+        if (IsBusy)
+            return;
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if(Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Unable to get monkeys: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+
     }
 }
